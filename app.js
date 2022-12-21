08940a40d7eec953e45d404b3ba6d7fce5550ac0
wanliu <1692894189@qq.com>
2022-12-21 16:27:51 +0800

    feat: 全局对象

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const http = require("http");
+console.log(__dirname);
+console.log(__filename);
+// __dirname和__filename是全局可用对象，就是在任何模块都可用，但是不是属于global的对象
+global.console.log("asdf"); // console就是全局对象
+console.log("asdf");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
-});
+// const Circle = require("./circle");
 
-server.listen(3000);
+// const circle = new Circle();
+
+// console.log(circle.area(5));
+// console.log(circle.circumference(7));
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   res.end(
+//     JSON.stringify({
+//       data: "Hello World!",
+//     })
+//   );
+// });
+
+// server.listen(3000);
